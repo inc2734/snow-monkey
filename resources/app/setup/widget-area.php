e5e358ad0366be58ddc66261f47d16a51cd5a0a5
Takashi Kitajima <inc@2inc.org>
2017-09-01 15:32:24 +0900

    Update sidebar widget area

--- resources/app/setup/widget-area.php
+++ resources/app/setup/widget-area.php
@@ -11,15 +11,25 @@
  * @return void
  */
 add_action( 'widgets_init', function() {
-	register_sidebar( [
-		'name'          => __( 'Sidebar', 'snow-monkey' ),
-		'id'            => 'sidebar-widget-area',
-		'description'   => __( 'This widgets are displayed in the sidebar of singular pages.', 'snow-monkey' ),
-		'before_widget' => '<div id="%1$s" class="c-widget %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h2 class="c-widget__title"><span>',
-		'after_title'   => '</span></h2>',
+	$post_types = get_post_types( [
+		'public' => true,
 	] );
+	unset( $post_types['attachment'] );
+	foreach ( $post_types as $post_type ) {
+		$post_type_object = get_post_type_object( $post_type );
+
+		register_sidebar( [
+			// @codingStandardsIgnoreStart
+			'name'          => sprintf( __( '%1$s sidebar', 'snow-monkey' ), __( $post_type_object->label ) ),
+			'description'   => sprintf( __( 'This widgets are displayed in the %1$s page sidebar.', 'snow-monkey' ), __( $post_type_object->label ) ),
+			// @codingStandardsIgnoreEnd
+			'id'            => $post_type_object->name . '-post-type-sidebar-widget-area',
+			'before_widget' => '<div id="%1$s" class="c-widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h2 class="c-widget__title"><span>',
+			'after_title'   => '</span></h2>',
+		] );
+	}
 } );
 
 /**
@@ -31,7 +41,7 @@ add_action( 'widgets_init', function() {
 	register_sidebar( [
 		'name'          => __( 'Archive sidebar', 'snow-monkey' ),
 		'id'            => 'archive-sidebar-widget-area',
-		'description'   => __( 'This widgets are displayed in the sidebar of archive pages.', 'snow-monkey' ),
+		'description'   => __( 'This widgets are displayed in the archive page sidebar.', 'snow-monkey' ),
 		'before_widget' => '<div id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h2 class="c-widget__title"><span>',
@@ -48,7 +58,7 @@ add_action( 'widgets_init', function() {
 	register_sidebar( [
 		'name'          => __( 'Footer', 'snow-monkey' ),
 		'id'            => 'footer-widget-area',
-		'description'   => __( 'This widgets are displayed in the footer of all pages.', 'snow-monkey' ),
+		'description'   => __( 'This widgets are displayed in the footer.', 'snow-monkey' ),
 		'before_widget' => '<div class="l-footer-widget-area__item c-row__col c-row__col--1-1 c-row__col--lg-' . esc_attr( get_theme_mod( 'footer-widget-area-column-size' ) ) . '"><div id="%1$s" class="c-widget %2$s">',
 		'after_widget'  => '</div></div>',
 		'before_title'  => '<h2 class="c-widget__title">',
--- resources/template-parts/sidebar-widget-area.php
+++ resources/template-parts/sidebar-widget-area.php
@@ -5,11 +5,11 @@
  * @license GPL-2.0+
  */
 
-if ( ! is_active_sidebar( 'sidebar-widget-area' ) ) {
+if ( ! is_active_sidebar( get_post_type() . '-post-type-sidebar-widget-area' ) ) {
 	return;
 }
 ?>
 
 <div class="l-sidebar-widget-area">
-	<?php dynamic_sidebar( 'sidebar-widget-area' ); ?>
+	<?php dynamic_sidebar( get_post_type() . '-post-type-sidebar-widget-area' ); ?>
 </div>
