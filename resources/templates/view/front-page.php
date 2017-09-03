6aa058ffe86908d08c6141ac2ef3c76606d048b9
Takashi Kitajima <inc@2inc.org>
2017-09-03 14:11:37 +0900

    Remove fornt page content

--- resources/templates/view/front-page.php
+++ resources/templates/view/front-page.php
@@ -8,16 +8,6 @@
 <article <?php post_class(); ?>>
 	<?php get_template_part( 'template-parts/main-visual' ); ?>
 
-	<?php if ( get_the_content() ) : ?>
-		<div class="p-front-page-content">
-			<div class="c-entry">
-				<div class="c-entry__content">
-					<?php the_content(); ?>
-				</div>
-			</div>
-		</div>
-	<?php endif; ?>
-
 	<?php get_template_part( 'template-parts/front-page-widget-area-top' ); ?>
 
 	<?php get_template_part( 'template-parts/recent-posts' ); ?>
