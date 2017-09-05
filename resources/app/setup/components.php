cae00f13010aac9bb2a0206f6713f1b7a7dd26da
Takashi Kitajima <inc@2inc.org>
2017-09-05 22:42:30 +0900

    Some fixes

--- resources/app/setup/components.php
+++ resources/app/setup/components.php
@@ -5,15 +5,6 @@
  * @license GPL-2.0+
  */
 
-/*
 use Inc2734\WP_Awesome_Components\Awesome_Components;
 
-if ( class_exists( 'Awesome_Components' ) ) {
-	return;
-}
-
 new Awesome_Components();
-*/
-
-include_once( get_theme_file_path( '/../vendor/inc2734/wp-awesome-components/src/wp-awesome-components.php' ) );
-new Inc2734_WP_Awesome_Components();
