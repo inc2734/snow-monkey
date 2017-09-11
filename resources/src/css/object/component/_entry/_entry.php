e23a8e131b5f91c7509b187f4e4d7f184fbb96f9
Takashi Kitajima <inc@2inc.org>
2017-09-11 15:51:05 +0900

    Some fixes

--- resources/src/css/object/component/_entry/_entry.php
+++ resources/src/css/object/component/_entry/_entry.php
@@ -10,14 +10,15 @@ use Inc2734\WP_Customizer_Framework\Customizer_Framework;
 $cfs = Customizer_Framework::styles();
 
 $accent_color = get_theme_mod( 'accent-color' );
+$selector = ( ! is_admin() ) ? '.c-entry__content' : '';
 
 $cfs->register(
-	( ! is_admin() ) ? '.c-entry__content > ' : '' . ' > h2::after',
+	$selector . ' > h2::after',
 	'background-color: ' . $accent_color
 );
 
 $cfs->register(
-	( ! is_admin() ) ? '.c-entry__content' : '' . ' > table thead th',
+	$selector . ' > table thead th',
 	[
 		'background-color: ' . $accent_color,
 		'border-right-color: ' . $cfs->light( $accent_color ),
