72c4d09b68a5d9ed559b5cd9c96e98b0a77262c1
Takashi Kitajima <inc@2inc.org>
2017-10-09 15:45:30 +0900

    Fix #9

--- resources/app/setup/updater.php
+++ resources/app/setup/updater.php
@@ -7,4 +7,4 @@
 
 use Inc2734\WP_GitHub_Theme_Updater\GitHub_Theme_Updater;
 
-$updater = new GitHub_Theme_Updater( get_stylesheet(), 'inc2734', 'snow-monkey' );
+$updater = new GitHub_Theme_Updater( get_template(), 'inc2734', 'snow-monkey' );
