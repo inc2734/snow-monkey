f6d084dcc867c37620acbcf3f7c5dea8601e8018
Takashi Kitajima <inc@2inc.org>
2017-10-14 03:23:30 +0900

    #3 Disable pure css gallery when jetpack carousel or tiled-gallery enabled

--- resources/app/setup/gallery.php
+++ resources/app/setup/gallery.php
@@ -7,4 +7,11 @@
 
 use Inc2734\WP_Pure_CSS_Gallery\Pure_CSS_Gallery;
 
+if ( class_exists( 'Jetpack' ) ) {
+	$jetpack_active_modules = \Jetpack::get_active_modules();
+	if ( in_array( 'carousel',  $jetpack_active_modules ) || in_array( 'tiled-gallery', $jetpack_active_modules ) ) {
+		return;
+	}
+}
+
 new Pure_CSS_Gallery();
