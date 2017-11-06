55f171456ffbf4dc2c87f833dd6e888eb4ddc90e
Takashi Kitajima <inc@2inc.org>
2017-11-06 11:15:59 +0900

    Some fixes

--- resources/app/setup/gallery.php
+++ resources/app/setup/gallery.php
@@ -9,7 +9,7 @@ use Inc2734\WP_Pure_CSS_Gallery\Pure_CSS_Gallery;
 
 if ( class_exists( 'Jetpack' ) ) {
 	$jetpack_active_modules = \Jetpack::get_active_modules();
-	if ( in_array( 'carousel',  $jetpack_active_modules ) || in_array( 'tiled-gallery', $jetpack_active_modules ) ) {
+	if ( in_array( 'carousel', $jetpack_active_modules ) || in_array( 'tiled-gallery', $jetpack_active_modules ) ) {
 		return;
 	}
 }
