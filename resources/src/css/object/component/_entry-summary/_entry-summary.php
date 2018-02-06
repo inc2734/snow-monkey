279db6781744f59cd1fd579fc27d1b7058a69577
Takashi Kitajima <inc@2inc.org>
2018-02-06 16:06:38 +0900

    Fix #107

--- resources/src/css/object/component/_entry-summary/_entry-summary.php
+++ resources/src/css/object/component/_entry-summary/_entry-summary.php
@@ -12,7 +12,10 @@ $cfs = Customizer_Framework::styles();
 $accent_color = get_theme_mod( 'accent-color' );
 
 $cfs->register(
-	'.c-entries--rich-media .c-entry-summary__figure::after',
+	[
+		'.c-entries--rich-media .c-entry-summary__figure::after',
+		'.c-entries--rich-media .c-page-summary__figure::after',
+	],
 	[
 		'background-color: ' . $cfs->rgba( $accent_color, .4 ),
 		'background-image: radial-gradient(' . $cfs->rgba( $accent_color, .9 ) . ' 33%, transparent 33%)',
