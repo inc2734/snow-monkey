62d93ed82a1a6b876368cab02b36630a5fad47ad
Takashi Kitajima <inc@2inc.org>
2018-02-08 02:15:25 +0900

    Change widget area panel position

--- resources/app/setup/widget-area.php
+++ resources/app/setup/widget-area.php
@@ -195,23 +195,6 @@ add_action( 'widgets_init', function() {
 } );
 
 /**
- * Add footer widget area
- *
- * @return void
- */
-add_action( 'widgets_init', function() {
-	register_sidebar( [
-		'name'          => __( 'Footer', 'snow-monkey' ),
-		'id'            => 'footer-widget-area',
-		'description'   => __( 'This widgets are displayed in the footer.', 'snow-monkey' ),
-		'before_widget' => '<div class="l-footer-widget-area__item c-row__col c-row__col--1-1 c-row__col--lg-1-1"><div id="%1$s" class="c-widget %2$s">',
-		'after_widget'  => '</div></div>',
-		'before_title'  => '<h2 class="c-widget__title"><span>',
-		'after_title'   => '</span></h2>',
-	] );
-} );
-
-/**
  * Add front page widget area
  *
  * @return void
@@ -271,4 +254,21 @@ add_action( 'widgets_init', function() {
 
 		return $params;
 	} );
+} );
+
+/**
+ * Add footer widget area
+ *
+ * @return void
+ */
+add_action( 'widgets_init', function() {
+	register_sidebar( [
+		'name'          => __( 'Footer', 'snow-monkey' ),
+		'id'            => 'footer-widget-area',
+		'description'   => __( 'This widgets are displayed in the footer.', 'snow-monkey' ),
+		'before_widget' => '<div class="l-footer-widget-area__item c-row__col c-row__col--1-1 c-row__col--lg-1-1"><div id="%1$s" class="c-widget %2$s">',
+		'after_widget'  => '</div></div>',
+		'before_title'  => '<h2 class="c-widget__title"><span>',
+		'after_title'   => '</span></h2>',
+	] );
 } );
