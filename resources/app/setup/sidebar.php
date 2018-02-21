92cb3e5db7abfdf7b50501a2c3c51435a8e42121
Takashi Kitajima <inc@2inc.org>
2018-02-21 22:27:19 +0900

    Add action hook snow_monkey_sidebar

---
+++ resources/app/setup/sidebar.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package snow-monkey
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+
+/**
+ * Add google adsense to sidebar
+ *
+ * @return void
+ */
+function snow_monkey_sidebar_add_google_adsense() {
+	wpvc_get_template_part( 'template-parts/google-adsense', [
+		'position' => 'sidebar-top',
+	] );
+}
+add_action( 'snow_monkey_sidebar', 'snow_monkey_sidebar_add_google_adsense', 10 );
+
+/**
+ * Add widget area to sidebar
+ *
+ * @return void
+ */
+function snow_monkey_sidebar_add_sidebar_widget_area() {
+	if ( ( ! is_front_page() && is_home() ) || is_archive() || is_search() ) {
+		get_template_part( 'template-parts/archive-sidebar-widget-area' );
+	} else {
+		get_template_part( 'template-parts/sidebar-widget-area' );
+		get_template_part( 'template-parts/sidebar-sticky-widget-area' );
+	}
+}
+add_action( 'snow_monkey_sidebar', 'snow_monkey_sidebar_add_sidebar_widget_area', 20 );
--- resources/templates/layout/sidebar/sidebar.php
+++ resources/templates/layout/sidebar/sidebar.php
@@ -5,13 +5,4 @@
  * @license GPL-2.0+
  */
 
-wpvc_get_template_part( 'template-parts/google-adsense', [
-	'position' => 'sidebar-top',
-] );
-
-if ( ( ! is_front_page() && is_home() ) || is_archive() || is_search() ) {
-	get_template_part( 'template-parts/archive-sidebar-widget-area' );
-} else {
-	get_template_part( 'template-parts/sidebar-widget-area' );
-	get_template_part( 'template-parts/sidebar-sticky-widget-area' );
-}
+do_action( 'snow_monkey_sidebar' );
