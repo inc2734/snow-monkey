db0b2116433b4a30e35567827df312f16d559c0e
Takashi Kitajima <inc@2inc.org>
2018-03-06 13:19:55 +0900

    Fix #139

--- resources/template-parts/related-posts.php
+++ resources/template-parts/related-posts.php
@@ -4,13 +4,23 @@
  * @author inc2734
  * @license GPL-2.0+
  */
+
+$matched_content     = get_option( 'mwt-google-matched-content' );
+$use_matched_content = (bool) $matched_content;
 ?>
 
 <aside class="p-related-posts c-entry-aside">
-	<h2 class="p-related-posts__title c-entry-aside__title"><span><?php esc_html_e( 'Related posts', 'snow-monkey' ); ?></span></h2>
+	<h2 class="p-related-posts__title c-entry-aside__title">
+		<span>
+			<?php esc_html_e( 'Related posts', 'snow-monkey' ); ?>
+			<?php if ( $use_matched_content ) : ?>
+				<?php esc_html_e( '(Including some ads)', 'snow-monkey' ); ?>
+			<?php endif; ?>
+		</span>
+	</h2>
+
 	<ul class="c-entries c-entries--<?php echo esc_attr( get_theme_mod( 'archive-layout' ) ); ?>">
-		<?php $matched_content = get_option( 'mwt-google-matched-content' ); ?>
-		<?php if ( $matched_content ) : ?>
+		<?php if ( $use_matched_content ) : ?>
 
 			<?php \Inc2734\WP_Awesome_Widgets\inc2734_wpaw_display_adsense_code( $matched_content ); ?>
 
