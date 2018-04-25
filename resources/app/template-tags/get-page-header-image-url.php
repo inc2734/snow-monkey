cd7ebdbbf1eddd87671433a602ca86b2305adbd2
Takashi Kitajima <inc@2inc.org>
2018-04-25 23:43:39 +0900

    Fix #177

--- resources/app/template-tags/get-page-header-image-url.php
+++ resources/app/template-tags/get-page-header-image-url.php
@@ -13,5 +13,12 @@ use Snow_Monkey\app\model\Page_Header_Image_Url;
  * @return string
  */
 function snow_monkey_get_page_header_image_url() {
-	return Page_Header_Image_Url::get();
+	$url = apply_filters( 'snow_monkey_pre_page_header_image_url', null );
+	if ( $url ) {
+		return $url;
+	}
+
+	$url = Page_Header_Image_Url::get();
+
+	return apply_filters( 'snow_monkey_page_header_image_url', $url );
 }
