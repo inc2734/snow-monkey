66d8b89ca0362e328fb42c3ddb104b4a119ece2a
Takashi Kitajima <inc@2inc.org>
2018-06-14 15:23:03 +0900

    Fix #220 #218 #217 #186

---
+++ resources/app/setup/woocommerce.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package snow-monkey
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+
+add_action( 'after_setup_theme', function() {
+	add_theme_support( 'woocommerce' );
+} );
--- resources/template-parts/hamburger-btn.php
+++ resources/template-parts/hamburger-btn.php
@@ -6,8 +6,12 @@
  */
 ?>
 <button id="hamburger-btn" class="c-hamburger-btn" aria-expanded="false" aria-controls="drawer-nav">
-	<div class="c-hamburger-btn__bar"></div>
-	<div class="c-hamburger-btn__bar"></div>
-	<div class="c-hamburger-btn__bar"></div>
-	<div class="screen-reader-text"><?php esc_html_e( 'Menu', 'snow-monkey' ); ?></div>
+	<div class="c-hamburger-btn__bars">
+		<div class="c-hamburger-btn__bar"></div>
+		<div class="c-hamburger-btn__bar"></div>
+		<div class="c-hamburger-btn__bar"></div>
+	</div>
+	<div class="c-hamburger-btn__label">
+		<?php esc_html_e( 'MENU', 'snow-monkey' ); ?>
+	</div>
 </button>
