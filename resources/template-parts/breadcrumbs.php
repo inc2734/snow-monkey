3a6fa9489eeb0945197c05dc302b88fffc0fecef
Takashi Kitajima <inc@2inc.org>
2018-07-02 14:28:43 +0900

    Fix #239

--- resources/template-parts/breadcrumbs.php
+++ resources/template-parts/breadcrumbs.php
@@ -20,11 +20,11 @@ $items = apply_filters( 'snow_monkey_breadcrumbs', $breadcrumbs->get() );
 		<li class="c-breadcrumbs__item" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
 			<?php if ( empty( $item['link'] ) ) : ?>
 				<span itemscope itemtype="http://schema.org/Thing" itemprop="item">
-					<span itemprop="name"><?php echo esc_html( $item['title'] ); ?></span>
+					<span itemprop="name"><?php echo esc_html( strip_tags( $item['title'] ) ); ?></span>
 				</span>
 			<?php else : ?>
 				<a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="<?php echo esc_url( $item['link'] ); ?>">
-					<span itemprop="name"><?php echo esc_html( $item['title'] ); ?></span>
+					<span itemprop="name"><?php echo esc_html( strip_tags( $item['title'] ) ); ?></span>
 				</a>
 			<?php endif; ?>
 			<meta itemprop="position" content="<?php echo esc_attr( $key + 1 ); ?>" />
