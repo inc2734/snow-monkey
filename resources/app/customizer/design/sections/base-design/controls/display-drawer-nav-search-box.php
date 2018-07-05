75669fefce86197f569ca0ae7a706bbd968aa948
Takashi Kitajima <inc@2inc.org>
2018-07-05 11:43:29 +0900

    Fix #247

---
+++ resources/app/customizer/design/sections/base-design/controls/display-drawer-nav-search-box.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package snow-monkey
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+
+use Inc2734\WP_Customizer_Framework\Customizer_Framework;
+
+$customizer = Customizer_Framework::init();
+$panel      = $customizer->get_panel( 'design' );
+$section    = $customizer->get_section( 'base-design' );
+
+$customizer->control( 'checkbox', 'display-drawer-nav-search-box', [
+	'transport' => 'postMessage',
+	'label'     => __( 'Display the search box in drawer navigation', 'snow-monkey' ),
+	'priority'  => 170,
+	'default'   => true,
+] );
+
+$control = $customizer->get_control( 'display-drawer-nav-search-box' );
+$control->join( $section )->join( $panel );
+$control->partial( [
+	'selector'        => '#drawer-nav .p-search-form',
+	'active_callback' => function() {
+		return has_nav_menu( 'drawer-nav' );
+	},
+] );
--- resources/template-parts/drawer-nav.php
+++ resources/template-parts/drawer-nav.php
@@ -11,6 +11,16 @@ if ( ! has_nav_menu( 'drawer-nav' ) ) {
 ?>
 
 <nav id="drawer-nav" class="c-drawer c-drawer--fixed" role="navigation" aria-hidden="true" aria-labelledby="hamburger-btn">
+	<?php do_action( 'snow_monkey_prepend_drawer_nav' ); ?>
+
+	<?php if ( get_theme_mod( 'display-drawer-nav-search-box' ) ) : ?>
+		<ul class="c-drawer__menu">
+			<li class="c-drawer__item">
+				<?php get_search_form( true ); ?>
+			</li>
+		</ul>
+	<?php endif; ?>
+
 	<?php
 	wp_nav_menu( [
 		'theme_location' => 'drawer-nav',
@@ -19,4 +29,6 @@ if ( ! has_nav_menu( 'drawer-nav' ) ) {
 		'depth'          => 0,
 	] );
 	?>
+
+	<?php do_action( 'snow_monkey_append_drawer_nav' ); ?>
 </nav>
