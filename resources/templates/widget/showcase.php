41bd10e07e5814ff73fbd86167ef50e798a5af5f
Takashi Kitajima <inc@2inc.org>
2018-07-10 12:37:16 +0900

    Fix #258

--- resources/templates/widget/showcase.php
+++ resources/templates/widget/showcase.php
@@ -51,7 +51,7 @@
 						</div>
 					<?php endif; ?>
 
-					<?php if ( ! empty( $instance['link-url'] ) && ! empty( $instance['link-text'] ) ) : ?>
+					<?php if ( ! $is_block_link && ! empty( $instance['link-url'] ) && ! empty( $instance['link-text'] ) ) : ?>
 						<div class="wpaw-showcase__action">
 							<a class="wpaw-showcase__more" href="<?php echo esc_url( $instance['link-url'] ); ?>">
 								<?php echo esc_html( $instance['link-text'] ); ?>
@@ -64,8 +64,15 @@
 					<div class="wpaw-showcase__figure">
 						<?php
 						$thumbnail_size = apply_filters( 'inc2734_wp_awesome_widgets_showcase_image_size', 'large', wp_is_mobile(), $args['widget_id'] );
+						$thumbnail      = wp_get_attachment_image_url( $instance['thumbnail'], $thumbnail_size );
 						?>
-						<img src="<?php echo esc_url( wp_get_attachment_image_url( $instance['thumbnail'], $thumbnail_size ) ); ?>" alt="">
+						<?php if ( ! $is_block_link && ! empty( $instance['link-url'] ) ) : ?>
+							<a href="<?php echo esc_url( $instance['link-url'] ); ?>">
+								<img src="<?php echo esc_url( $thumbnail ); ?>" alt="">
+							</a>
+						<?php else : ?>
+							<img src="<?php echo esc_url( $thumbnail ); ?>" alt="">
+						<?php endif; ?>
 					</div>
 				<?php endif; ?>
 
