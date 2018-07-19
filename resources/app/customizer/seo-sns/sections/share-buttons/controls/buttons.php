28ea95566ca5ff895a8a355ab2080d1cbf56fee4
Takashi Kitajima <inc@2inc.org>
2018-07-19 14:31:21 +0900

    Fix #261

--- resources/app/customizer/seo-sns/sections/share-buttons/controls/buttons.php
+++ resources/app/customizer/seo-sns/sections/share-buttons/controls/buttons.php
@@ -17,13 +17,14 @@ $customizer->control( 'multiple-checkbox', 'mwt-share-buttons-buttons', [
 	'priority' => 100,
 	'default'  => '',
 	'choices'  => [
-		'facebook' => __( 'Facebook', 'snow-monkey' ),
-		'twitter'  => __( 'Twitter', 'snow-monkey' ),
-		'hatena'   => __( 'Hatena Bookmark', 'snow-monkey' ),
-		'feedly'   => __( 'Feedly', 'snow-monkey' ),
-		'line'     => __( 'LINE', 'snow-monkey' ),
-		'pocket'   => __( 'Pocket', 'snow-monkey' ),
-		'feed'     => __( 'Feed', 'snow-monkey' ),
+		'facebook'    => __( 'Facebook', 'snow-monkey' ),
+		'twitter'     => __( 'Twitter', 'snow-monkey' ),
+		'hatena'      => __( 'Hatena Bookmark', 'snow-monkey' ),
+		'google_plus' => __( 'Google+', 'snow-monkey' ),
+		'feedly'      => __( 'Feedly', 'snow-monkey' ),
+		'line'        => __( 'LINE', 'snow-monkey' ),
+		'pocket'      => __( 'Pocket', 'snow-monkey' ),
+		'feed'        => __( 'Feed', 'snow-monkey' ),
 	],
 ] );
 
