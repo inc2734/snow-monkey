37a676a2f6c9a852ad2c51a5515b4c281bd96ff0
Takashi Kitajima <inc@2inc.org>
2018-08-17 22:23:42 +0900

    Update inc2734/wp-page-speed-optimization

--- resources/app/setup/page-speed-optimization.php
+++ resources/app/setup/page-speed-optimization.php
@@ -22,6 +22,8 @@ add_action( 'after_setup_theme', function() {
 	add_filter( 'inc2734_wp_page_speed_optimization_defer_scripts', function( $handles ) {
 		return array_merge( $handles, [
 			snow_monkey_get_main_script_handle(),
+			snow_monkey_get_main_script_handle() . '-wp-pure-css-gallery',
+			'wp-oembed-blog-card',
 		] );
 	} );
 
