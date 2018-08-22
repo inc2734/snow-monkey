bc4132926837e70319de38bc223f744e14294cb0
Takashi Kitajima <inc@2inc.org>
2018-08-22 13:37:12 +0900

    Some fixes for Gutenberg

--- resources/app/setup/image-size.php
+++ resources/app/setup/image-size.php
@@ -8,6 +8,18 @@
 add_image_size( 'xlarge', 1920, 1920 );
 
 /**
+ * Add xlarge on media uploader and wp.media.sizes
+ *
+ * @param array $sizes
+ * @return array
+ */
+add_filter( 'image_size_names_choose', function( $sizes ) {
+	return array_merge( $sizes, [
+		'xlarge' => __( 'xLarge', 'snow-monkey' ),
+	] );
+} );
+
+/**
  * Update pickup slider widget image size
  */
 add_filter( 'inc2734_wp_awesome_widgets_pickup_slider_image_size', function( $thumbnail_size, $is_mobile ) {
