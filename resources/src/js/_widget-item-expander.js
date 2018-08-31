b1e1c7bc2bd64cb6a1cc9dfeaea08332a83ba735
Takashi Kitajima <inc@2inc.org>
2018-08-31 15:02:27 +0900

    Refactoring SnowMonkeyWidgetItemExpander

--- resources/src/js/_widget-item-expander.js
+++ resources/src/js/_widget-item-expander.js
@@ -1,27 +1,54 @@
 'use strict';
 
-import $ from 'jquery';
-
 export default class SnowMonkeyWidgetItemExpander {
 
   constructor() {
-    const parents = $('.c-widget li:has(.children, .sub-menu)');
+    window.addEventListener('DOMContentLoaded', () => this._DOMContentLoaded(), false);
+  }
 
-    parents.each((i, e) => {
-      const parent = $(e);
+  _DOMContentLoaded() {
+    const children = document.querySelectorAll('.c-widget .children, .c-widget .sub-menu');
 
-      parent.prepend('<button class="children-expander" data-is-expanded="false"></button>');
-      parent.find('.children, .sub-menu').attr('data-is-hidden', 'true');
+    this._forEachHtmlNodes(children, (element) => {
+      element.setAttribute('data-is-hidden', 'true');
 
-      parent.children('.children-expander').click((event) => {
-        if ('false' === $(event.target).attr('data-is-expanded')) {
-          $(event.target).attr('data-is-expanded', 'true');
-          parent.children('.children, .sub-menu').attr('data-is-hidden', 'false');
-        } else {
-          parent.find('.children-expander').attr('data-is-expanded', 'false');
-          parent.find('.children, .sub-menu').attr('data-is-hidden', 'true');
+      const parent = element.parentNode;
+      const btn = document.createElement('button');
+      btn.classList.add('children-expander');
+      btn.setAttribute('data-is-expanded', 'false');
+
+      parent.insertBefore(btn, parent.firstElementChild);
+
+      btn.addEventListener('click', (event) => this._click(event), false);
+    });
+  }
+
+  _click(event) {
+    const btn = event.currentTarget;
+    const parent = btn.parentNode;
+
+    if ('false' === btn.getAttribute('data-is-expanded')) {
+      btn.setAttribute('data-is-expanded', 'true');
+
+      this._forEachHtmlNodes(parent.children, (element) => {
+        if ('true' === element.getAttribute('data-is-hidden')) {
+          element.setAttribute('data-is-hidden', 'false');
         }
       });
-    });
+    } else {
+      this._forEachHtmlNodes(parent.getElementsByClassName('children-expander'), (element) => {
+        element.setAttribute('data-is-expanded', 'false');
+      });
+
+      this._forEachHtmlNodes(parent.querySelectorAll('.children, .sub-menu'), (element) => {
+        element.setAttribute('data-is-hidden', 'true');
+      });
+    }
+  }
+
+  _forEachHtmlNodes(htmlNodes, callback) {
+    if (0 < htmlNodes.length) {
+      [].forEach.call(htmlNodes, (htmlNode) => callback(htmlNode));
+    }
   }
 }
