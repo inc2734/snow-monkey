eeb73722fe5931a9cbc13dac9228385ac14f9640
Takashi Kitajima <inc@2inc.org>
2018-09-14 15:36:13 +0900

    Refactoring c-prev-next-nav

--- resources/template-parts/prev-next-nav.php
+++ resources/template-parts/prev-next-nav.php
@@ -4,70 +4,60 @@
  * @author inc2734
  * @license GPL-2.0+
  */
-
-$prev_post = get_previous_post();
-$next_post = get_next_post();
-
-if ( ! $prev_post && ! $next_post ) {
-	return;
-}
-
-$prev_next_posts = [
-	'next' => $prev_post,
-	'prev' => $next_post,
-];
 ?>
-
 <div class="c-prev-next-nav">
-	<?php foreach ( $prev_next_posts as $key => $_post ) : ?>
+	<?php foreach ( [ 'next', 'prev' ] as $key ) : ?>
 		<div class="c-prev-next-nav__item c-prev-next-nav__item--<?php echo esc_attr( $key ); ?>">
-			<?php if ( $_post ) : ?>
-				<?php
-				$prev_next_query = new WP_Query(
-					[
-						'p'           => $_post->ID,
-						'post_status' => $_post->post_status,
-						'post_type'   => $_post->post_type,
-					]
+			<?php
+			if ( 'next' === $key ) {
+				$_post = get_previous_post();
+			} elseif ( 'prev' === $key ) {
+				$_post = get_next_post();
+			}
+
+			$background_image_size = wp_is_mobile() ? 'large' : 'medium';
+			$background_image_url = wp_get_attachment_image_url( get_post_thumbnail_id( $_post->ID ), $background_image_size );
+
+			ob_start();
+			?>
+			<div class="c-prev-next-nav__item-figure"
+				style="background-image: url(<?php echo esc_url( $background_image_url ); ?>)"
+			></div>
+			<div class="c-prev-next-nav__item-label">
+				<?php if ( 'next' === $key ) : ?>
+					<i class="fas fa-angle-left" aria-hidden="true"></i>
+					<?php esc_html_e( 'Old post', 'snow-monkey' ); ?>
+				<?php else : ?>
+					<?php esc_html_e( 'New post', 'snow-monkey' ); ?>
+					<i class="fas fa-angle-right" aria-hidden="true"></i>
+				<?php endif; ?>
+			</div>
+			<div class="c-prev-next-nav__item-title">
+				%title
+			</div>
+			<?php
+			$format = ob_get_clean();
+
+			function snow_monkey_prev_next_nav_title( $nav_title ) {
+				return wp_trim_words( $nav_title, class_exists( 'multibyte_patch' ) ? 30 : 60 );
+			}
+			add_filter( 'the_title', 'snow_monkey_prev_next_nav_title' );
+
+			if ( 'next' === $key ) {
+				previous_post_link(
+					'%link',
+					$format
 				);
-				?>
-				<?php while ( $prev_next_query->have_posts() ) : ?>
-					<?php
-					$prev_next_query->the_post();
+			} elseif ( 'prev' === $key ) {
+				next_post_link(
+					'%link',
+					$format
+				);
+			}
 
-					$background_image_size = 'medium';
-					if ( ! wp_is_mobile() ) {
-						$background_image_size = 'large';
-					}
-					$background_image_url = wp_get_attachment_image_url( get_post_thumbnail_id(), $background_image_size );
-					?>
-
-					<a href="<?php the_permalink(); ?>">
-						<div class="c-prev-next-nav__item-figure"
-							style="background-image: url(<?php echo esc_url( $background_image_url ); ?>)"
-						></div>
-						<div class="c-prev-next-nav__item-label">
-							<?php if ( 'next' === $key ) : ?>
-								<i class="fas fa-angle-left" aria-hidden="true"></i>
-								<?php esc_html_e( 'Old post', 'snow-monkey' ); ?>
-							<?php else : ?>
-								<?php esc_html_e( 'New post', 'snow-monkey' ); ?>
-								<i class="fas fa-angle-right" aria-hidden="true"></i>
-							<?php endif; ?>
-						</div>
-						<div class="c-prev-next-nav__item-title">
-							<?php
-							ob_start();
-							the_title();
-							$item_title = wp_trim_words( ob_get_clean(), class_exists( 'multibyte_patch' ) ? 30 : 60 );
-							echo esc_html( $item_title );
-							?>
-						</div>
-					</a>
-				<?php endwhile; ?>
-				<?php wp_reset_postdata(); ?>
-			<?php endif; ?>
+			remove_filter( 'the_title', 'snow_monkey_prev_next_nav_title' );
+			?>
 		</div>
 	<?php endforeach; ?>
-	<?php wp_reset_postdata(); ?>
 </div>
+<?php
