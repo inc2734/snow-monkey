2eff340f4b6864b818c2e9d952c43d651b854a89
Takashi Kitajima <inc@2inc.org>
2018-10-05 14:24:43 +0900

    Refactoring snow_monkey_get_page_templates()

--- resources/app/template-tags/get-page-templates.php
+++ resources/app/template-tags/get-page-templates.php
@@ -18,36 +18,132 @@ function snow_monkey_get_page_templates() {
 		return $wrappers;
 	}
 
-	foreach ( wpvc_config( 'layout' ) as $wrapper_dirs ) {
-		foreach ( glob( get_theme_file_path( $wrapper_dirs . '/*' ) ) as $file ) {
-			$name = rtrim( basename( $file ), '.php' );
-			if ( 'blank' === $name || 'blank-fluid' === $name || 'wrapper' === $name ) {
+	$page_templates = new Snow_Monkey_Page_Templates();
+	$wrappers = $page_templates->get();
+	return $wrappers;
+}
+
+class Snow_Monkey_Page_Templates {
+	/**
+	 * Available wrapper templates
+	 *
+	 * @var array
+	 */
+	protected $wrappers = [];
+
+	/**
+	 * Wrapper templates
+	 *
+	 * @var array
+	 */
+	protected $wrapper_files = [];
+
+	/**
+	 * Page templates
+	 *
+	 * @var array
+	 */
+	protected $page_template_files = [];
+
+	public function __construct() {
+		$this->wrapper_files = $this->_get_wrapper_files();
+		$this->page_template_files = $this->_get_page_template_files();
+	}
+
+	/**
+	 * Return available wrapper templates name
+	 *
+	 * @return array
+	 */
+	public function get() {
+		foreach ( $this->page_template_files as $name => $page_template_path ) {
+			if ( isset( $this->wrappers[ $name ] ) ) {
 				continue;
 			}
 
-			$page_template_path = null;
-			$template_name = wpvc_locate_template( (array) wpvc_config( 'page-templates' ), $name );
-			if ( $template_name ) {
-				$page_template_path = get_theme_file_path( $template_name . '.php' );
-
-				$page_template_data = get_file_data(
-					$page_template_path,
-					[
-						'template-name' => 'Template Name',
-					]
-				);
+			if ( ! isset( $this->wrapper_files[ $name ] ) ) {
+				continue;
 			}
 
-			$template_name = $name;
-			if ( ! empty( $page_template_data ) && ! empty( $page_template_data['template-name'] ) ) {
-				$template_name = $page_template_data['template-name'];
+			$label = $this->_get_template_label( $page_template_path );
+			if ( ! $label ) {
+				continue;
 			}
 
 			// @codingStandardsIgnoreStart
-			$wrappers[ $name ] = __( $template_name, 'snow-monkey' );
+			$this->wrappers[ $name ] = __( $label, 'snow-monkey' );
 			// @codingStandardsIgnoreEnd
 		}
+
+		return $this->wrappers;
 	}
 
-	return $wrappers;
+	/**
+	 * Return page template label
+	 *
+	 * @param string $path Page template path
+	 * @return string
+	 */
+	protected function _get_template_label( $path ) {
+		$page_template_data = get_file_data(
+			$path,
+			[
+				'template-name' => 'Template Name',
+			]
+		);
+
+		if ( empty( $page_template_data['template-name'] ) ) {
+			return;
+		}
+
+		return $page_template_data['template-name'];
+	}
+
+	/**
+	 * Return wrapper templates path
+	 *
+	 * @return array
+	 */
+	protected function _get_wrapper_files() {
+		$wrapper_files = [];
+
+		foreach ( wpvc_config( 'layout' ) as $wrapper_dir ) {
+			foreach ( glob( get_theme_file_path( $wrapper_dir . '/*' ) ) as $wrapper_path ) {
+				$name = basename( $wrapper_path, '.php' );
+				if ( 'blank' === $name || 'blank-fluid' === $name ) {
+					continue;
+				}
+				if ( isset( $wrapper_files[ $name ] ) ) {
+					continue;
+				}
+				$wrapper_files[ $name ] = $wrapper_path;
+			}
+		}
+
+		return $wrapper_files;
+	}
+
+	/**
+	 * Return page templates path
+	 *
+	 * @return array
+	 */
+	protected function _get_page_template_files() {
+		$page_template_files = [];
+
+		foreach ( wpvc_config( 'page-templates' ) as $page_template_dir ) {
+			foreach ( glob( get_theme_file_path( $page_template_dir . '/*' ) ) as $page_template_path ) {
+				$name = basename( $page_template_path, '.php' );
+				if ( 'blank' === $name || 'blank-fluid' === $name ) {
+					continue;
+				}
+				if ( isset( $page_template_files[ $name ] ) ) {
+					continue;
+				}
+				$page_template_files[ $name ] = $page_template_path;
+			}
+		}
+
+		return $page_template_files;
+	}
 }
