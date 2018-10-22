7035133eaeba64d74f733c67feba0bf5d04d56a9
Takashi Kitajima <inc@2inc.org>
2018-10-22 10:24:02 +0900

    Add design skin description in customizer

--- resources/app/customizer/design-skin/section.php
+++ resources/app/customizer/design-skin/section.php
@@ -16,7 +16,12 @@ $customizer = Customizer_Framework::init();
 $customizer->section(
 	'design-skin',
 	[
-		'title'    => __( 'Design skin', 'snow-monkey' ),
-		'priority' => 1030,
+		'title'       => __( 'Design skin', 'snow-monkey' ),
+		'priority'    => 1030,
+		'description' => sprintf(
+			__( 'Design skins can be downloaded from the %1$sdesign skin page%2$s.', 'snow-monkey' ),
+			'<a href="https://snow-monkey.2inc.org/design-skin/" target="_blank">',
+			'</a>'
+		),
 	]
 );
