7026c089f6a0f85fc9623aaecd9059e78d2dba4c
Takashi Kitajima <inc@2inc.org>
2018-10-27 13:48:35 +0900

    Update npm-scripts

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,9 @@ module.exports = {
     require('autoprefixer')({
       browsers: ['last 2 versions'],
       cascade: false
+    }),
+    require('cssnano')({
+      preset: 'default',
     })
   ]
 }
