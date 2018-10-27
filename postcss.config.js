7c0b8892661ca3af7fa0c45db9aea60ba7e9e516
Takashi Kitajima <inc@2inc.org>
2018-10-27 11:13:29 +0900

    Update npm packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
-module.exports = () => ({
-  plugins: {
-    autoprefixer: {
+module.exports = {
+  plugins: [
+    require('autoprefixer')({
       browsers: ['last 2 versions'],
       cascade: false
-    }
-  }
-});
+    })
+  ]
+}
