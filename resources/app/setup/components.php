36dd020484dd15b497651f4560095a3297cd3ff9
Takashi Kitajima <inc@2inc.org>
2018-11-01 17:18:15 +0900

    #361 for awesome components

--- resources/app/setup/components.php
+++ resources/app/setup/components.php
@@ -8,3 +8,32 @@
 use Inc2734\WP_Awesome_Components\Awesome_Components;
 
 new Awesome_Components();
+
+/**
+ * Awesome_Components の各コンポーネントをカスタマイズ
+ *
+ * @param array $components
+ * @return array
+ */
+add_filter(
+	'inc2734_wp_awesome_components_register_components',
+	function( $components ) {
+		foreach ( $components as $key => $component ) {
+			if ( 'btn' === $key || 'btn--full' === $key ) {
+				$component = str_replace( 'wpac-btn', 'wpac-btn c-btn', $component );
+				$components[ $key ] = $component;
+			} elseif ( 0 === strpos( $key, 'columns-' ) ) {
+				$component = str_replace( 'wpac-columns__col-inner', 'wpac-columns__col-inner p-entry-content', $component );
+				$components[ $key ] = $component;
+			} elseif ( 'section' === $key || 'section--inverse' === $key ) {
+				$component = str_replace( 'wpac-section ', 'c-section wpac-section ', $component );
+				$component = str_replace( 'wpac-section__body', 'wpac-section__body p-entry-content', $component );
+				$component = str_replace( 'wpac-section__body', 'wpac-section__body p-entry-content', $component );
+				$component = str_replace( 'wpac-section__container', 'c-container', $component );
+				$components[ $key ] = $component;
+			}
+		}
+		return $components;
+	},
+	11
+);
--- resources/app/setup/fallback.php
+++ resources/app/setup/fallback.php
@@ -5,6 +5,13 @@
  * @license GPL-2.0+
  */
 
+/**
+ * Add .p-entry-content to .c-entry__content
+ *
+ * @param string $content
+ * @param string $slug
+ * @return string
+ */
 add_filter(
 	'inc2734_view_controller_template_part_render',
 	function( $content, $slug ) {
@@ -17,23 +24,20 @@ add_filter(
 	2
 );
 
+/**
+ * Fallback for Awesome Components
+ *
+ * @param string $content
+ * @return string
+ */
 add_filter(
 	'the_content',
 	function( $content ) {
-		return str_replace( 'wpac-section__body', 'wpac-section__body p-*.scss', $content );
-	}
-);
-
-add_filter(
-	'the_content',
-	function( $content ) {
-		return str_replace( 'wpac-columns__col-inner', 'wpac-columns__col-inner p-entry-content', $content );
-	}
-);
-
-add_filter(
-	'the_content',
-	function( $content ) {
-		return str_replace( 'wpac-section__container', 'c-container', $content );
+		$content = str_replace( 'wpac-section ', 'c-section wpac-section ', $content );
+		$content = str_replace( 'wpac-section__body', 'wpac-section__body p-entry-content', $content );
+		$content = str_replace( 'wpac-columns__col-inner', 'wpac-columns__col-inner p-entry-content', $content );
+		$content = str_replace( 'wpac-section__container', 'c-container', $content );
+		$content = str_replace( 'wpac-btn', 'wpac-btn c-btn', $content );
+		return $content;
 	}
 );
