6ed5db901dcbae46e2eead2609a2509e03e094c3
Takashi Kitajima <inc@2inc.org>
2018-11-23 23:48:26 +0900

    Fix #383

--- resources/src/css/object/project/_entry-content/_entry-content.php
+++ resources/src/css/object/project/_entry-content/_entry-content.php
@@ -6,18 +6,23 @@
  */
 
 if ( is_admin() ) {
-	if ( function_exists( 'is_gutenberg_page' ) && is_gutenberg_page() ) {
-		snow_monkey_entry_content_styles(
-			[
-				'',
-				'[data-type="core/paragraph"] .components-autocomplete',
-				'[data-type="core/heading"] .components-autocomplete',
-				'.wp-block-freeform',
-			]
-		);
-	} else {
-		snow_monkey_entry_content_styles( [ '' ] );
-	}
+	add_action(
+		'admin_enqueue_scripts',
+		function() {
+			if ( function_exists( 'is_gutenberg_page' ) && is_gutenberg_page() ) {
+				snow_monkey_entry_content_styles(
+					[
+						'',
+						'[data-type="core/paragraph"] .components-autocomplete',
+						'[data-type="core/heading"] .components-autocomplete',
+						'.wp-block-freeform',
+					]
+				);
+			} else {
+				snow_monkey_entry_content_styles( [ '' ] );
+			}
+		}
+	);
 } else {
 	snow_monkey_entry_content_styles( [ '.p-entry-content' ] );
 }
