045d35de285c94a98dc3f28bb2af68edda4cdba7
Takashi Kitajima <inc@2inc.org>
2018-11-28 14:42:05 +0900

    Fix #390

--- resources/app/setup/entry-summary.php
+++ resources/app/setup/entry-summary.php
@@ -5,6 +5,26 @@
  * @license GPL-2.0+
  */
 
+/**
+ * Replace excerpt content when post_excerpt is empty
+ */
+remove_filter( 'get_the_excerpt', 'wp_trim_excerpt' );
+add_filter(
+	'get_the_excerpt',
+	function( $excerpt ) {
+		if ( $excerpt ) {
+			return $excerpt;
+		}
+		return \Inc2734\Mimizuku_Core\Helper\pure_trim_excerpt();
+	}
+);
+
+/**
+ * Set excerpt length
+ *
+ * @param int $length
+ * @return int
+ */
 add_filter(
 	'excerpt_length',
 	function( $length ) {
