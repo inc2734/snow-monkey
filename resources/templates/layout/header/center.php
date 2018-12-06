18e8d4f78f59190ba8bb709c9a0af4b91ced2c8e
Takashi Kitajima <inc@2inc.org>
2018-12-06 21:22:17 +0900

    #360 Change location of header template

--- resources/templates/layout/header/center.php
+++ resources/templates/layout/header/center.php
@@ -11,7 +11,7 @@ $header_position = snow_monkey_get_header_position();
 $default_header_position = snow_monkey_get_default_header_position();
 ?>
 <header class="l-header" role="banner" data-l-header-type="<?php echo esc_attr( $header_position ); ?>" data-snow-monkey-default-header-position="<?php echo esc_attr( $default_header_position ); ?>">
-	<?php Helper\get_template_part( 'template-parts/center-header' ); ?>
+	<?php Helper\get_template_part( 'template-parts/header/center' ); ?>
 
 	<?php if ( has_nav_menu( 'global-nav' ) ) : ?>
 		<div class="l-header__drop-nav" aria-hidden="true">
