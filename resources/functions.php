09ad9866a68401489b1842b28e6c41dd2d104f0a
Takashi Kitajima <inc@2inc.org>
2018-12-10 12:37:13 +0900

    auto-loader の最適化をすると resources のありなしでコケるので修正

--- resources/functions.php
+++ resources/functions.php
@@ -13,6 +13,24 @@ use Snow_Monkey\app\Helper;
 */
 require_once( get_template_directory() . '/vendor/autoload.php' );
 
+spl_autoload_register(
+	function( $class ) {
+		if ( 0 !== strpos( $class, 'Snow_Monkey' ) ) {
+			return;
+		}
+
+		$class = str_replace( 'Snow_Monkey', '', $class );
+		$class = str_replace( '\\', '/', $class );
+		$class_path = get_theme_file_path( $class . '.php' );
+
+		if ( ! file_exists( $class_path ) ) {
+			return;
+		}
+
+		require $class_path;
+	}
+);
+
 /**
  * Make theme available for translation
  *
