62ee2ad0aed968a4f3e7b2a65f123d72489ad63c
Takashi Kitajima <inc@2inc.org>
2018-12-10 11:40:35 +0900

    Some fixes

--- resources/template-parts/page-summary.php
+++ resources/template-parts/page-summary.php
@@ -5,25 +5,24 @@
  * @license GPL-2.0+
  */
 ?>
-	<section class="c-page-summary">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<a href="<?php the_permalink(); ?>">
-				<div class="c-page-summary__figure">
-					<?php the_post_thumbnail( 'xlarge' ); ?>
-				</div>
-			</a>
-		<?php endif; ?>
+<section class="c-page-summary">
+	<?php if ( has_post_thumbnail() ) : ?>
+		<a href="<?php the_permalink(); ?>">
+			<div class="c-page-summary__figure">
+				<?php the_post_thumbnail( 'xlarge' ); ?>
+			</div>
+		</a>
+	<?php endif; ?>
 
-		<header class="c-page-summary__header">
-			<h2 class="c-page-summary__title">
-				<?php the_title(); ?>
-			</h2>
-		</header>
-		<div class="c-page-summary__content">
-			<?php the_excerpt(); ?>
-		</div>
-		<div class="c-page-summary__action">
-			<a class="c-page-summary__more" href="<?php the_permalink(); ?>"><?php esc_html_e( 'More', 'snow-monkey' ); ?></a>
-		</div>
-	</section>
-</a>
+	<header class="c-page-summary__header">
+		<h2 class="c-page-summary__title">
+			<?php the_title(); ?>
+		</h2>
+	</header>
+	<div class="c-page-summary__content">
+		<?php the_excerpt(); ?>
+	</div>
+	<div class="c-page-summary__action">
+		<a class="c-page-summary__more" href="<?php the_permalink(); ?>"><?php esc_html_e( 'More', 'snow-monkey' ); ?></a>
+	</div>
+</section>
