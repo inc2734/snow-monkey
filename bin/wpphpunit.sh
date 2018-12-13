266cd1f63be86ab1c1aee3c4cf36fcca3abbd119
Takashi Kitajima <inc@2inc.org>
2018-12-13 12:17:59 +0900

    fix phpunit

--- bin/wpphpunit.sh
+++ bin/wpphpunit.sh
@@ -2,6 +2,9 @@
 
 set -e;
 
+WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
+WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress/}
+
 themedir=$(pwd)
 if [ ! -e resources/style.css ]; then
   echo 'Current directory is not a theme.'
@@ -14,12 +17,12 @@ cd ${themedir}
 if [ -e ${themedir}/bin/install-wp-tests.sh ]; then
   echo 'DROP DATABASE IF EXISTS wordpress_test;' | mysql -u root
 
-  if [ -e /tmp/wordpress ]; then
-    rm -fr /tmp/wordpress
+  if [ -e ${WP_CORE_DIR} ]; then
+    rm -fr ${WP_CORE_DIR}
   fi
 
-  if [ -e /tmp/wordpress-tests-lib ]; then
-    rm -fr /tmp/wordpress-tests-lib
+  if [ -e ${WP_TESTS_DIR} ]; then
+    rm -fr ${WP_TESTS_DIR}
   fi
 
   bash "${themedir}/bin/install-wp-tests.sh" wordpress_test root '' localhost latest;
