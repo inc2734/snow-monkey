97986d7100c5e8701dda949d05b64cc974729545
Takashi Kitajima <inc@2inc.org>
2018-12-18 15:16:49 +0900

    Fix #414

--- resources/src/js/_smooth-scroll.js
+++ resources/src/js/_smooth-scroll.js
@@ -3,7 +3,7 @@
 import $ from 'jquery';
 import '../../assets/packages/jquery.smoothscroll/src/jquery.smoothscroll.js';
 
-export default class SnowMonkeyPageTopScroll {
+export default class SnowMonkeySmoothScroll {
 
   constructor() {
     this.pageTop = $('.c-page-top');
@@ -47,29 +47,28 @@ export default class SnowMonkeyPageTopScroll {
       })()
     });
 
-    $('.wpco a[href^="#"]').SmoothScroll({
+    $('.wpco a[href^="#"], .u-smooth-scroll[href^="#"], .u-smooth-scroll a[href^="#"]').SmoothScroll({
       duration: 1000,
       easing  : 'easeOutQuint',
       offset  : () => {
-        const scroll            = $(window).scrollTop();
-        const adminBarHeight    = parseInt($('html').css('margin-top'));
-        const headerType        = $('.l-header').attr('data-l-header-type');
-        const getAdminBarHeight = () => {
-          if ('fixed' === $('#wpadminbar').css('position')) {
-            return parseInt($('html').css('margin-top'));
-          }
-          return 0;
-        }
-
-        if ('sticky' === headerType || 'overlay' === headerType) {
-          return $('.l-header').outerHeight() + getAdminBarHeight();
-        }
-
-        if ('false' === $('.l-header__drop-nav').attr('aria-hidden')) {
-          return $('.l-header__drop-nav .p-global-nav').outerHeight() + getAdminBarHeight();
-        }
-        return getAdminBarHeight();
+        return this._getAdminBarHeight();
       }
     });
+  }
+
+  _getAdminBarHeight() {
+    const scroll         = $(window).scrollTop();
+    const headerType     = $('.l-header').attr('data-l-header-type');
+    const adminBarHeight = ('fixed' === $('#wpadminbar').css('position')) ? parseInt($('html').css('margin-top')) : 0;
+
+    if ('sticky' === headerType || 'overlay' === headerType) {
+      return $('.l-header').outerHeight() + adminBarHeight;
+    }
+
+    if ('false' === $('.l-header__drop-nav').attr('aria-hidden')) {
+      return $('.l-header__drop-nav .p-global-nav').outerHeight() + adminBarHeight;
+    }
+
+    return adminBarHeight;
   }
 }
