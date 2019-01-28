32d412a7f5a34decb3e21146b11ec448bf0d5226
Takashi Kitajima <inc@2inc.org>
2019-01-28 15:11:18 +0900

    Fix #435

--- resources/app/setup/updater.php
+++ resources/app/setup/updater.php
@@ -5,6 +5,6 @@
  * @license GPL-2.0+
  */
 
-use Inc2734\WP_GitHub_Theme_Updater\GitHub_Theme_Updater;
+use Inc2734\WP_GitHub_Theme_Updater\Bootstrap;
 
-$updater = new GitHub_Theme_Updater( get_template(), 'inc2734', 'snow-monkey' );
+$updater = new Bootstrap( get_template(), 'inc2734', 'snow-monkey' );
