baed5d8ec2b28d698721ade25e5c4d93de584899
Takashi Kitajima <inc@2inc.org>
2019-02-01 15:34:48 +0900

    Fix #439 リファラに URL は含めないように変更

--- resources/app/setup/suppot-forum-widget.php
+++ resources/app/setup/suppot-forum-widget.php
@@ -11,18 +11,29 @@ add_action(
 			'snow-monkey-support-forum-topics-widget',
 			__( 'Recent topics of Snow Monkey support forum', 'snow-monkey' ),
 			function() {
+				global $wp_version;
+
 				$transient = get_transient( 'snow-monkey-support-forum-topics' );
+
 				if ( ! $transient ) {
-					$wp_api_posts = wp_remote_get( 'https://snow-monkey.2inc.org/wp-json/wp/v2/topic?per_page=5' );
+					$wp_api_posts = wp_remote_get(
+						'https://snow-monkey.2inc.org/wp-json/wp/v2/topic?per_page=5',
+						[
+							'user-agent' => 'WordPress/' . $wp_version,
+						]
+					);
+
 					if ( is_wp_error( $wp_api_posts ) || 200 !== $wp_api_posts['response']['code'] ) {
 						$wp_api_posts = [];
 					} else {
 						$wp_api_posts = json_decode( $wp_api_posts['body'] );
 					}
+
 					set_transient( 'snow-monkey-support-forum-topics', $wp_api_posts, 60 * 60 );
 				} else {
 					$wp_api_posts = $transient;
 				}
+
 				if ( ! $wp_api_posts ) {
 					return;
 				}
