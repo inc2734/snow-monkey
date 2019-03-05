76ae953fb5765a5fe7a693f3b20f3c8294113d0d
Takashi Kitajima <inc@2inc.org>
2019-03-05 16:33:20 +0900

    Update widget expander

--- resources/src/js/module/_widget-item-expander.js
+++ resources/src/js/module/_widget-item-expander.js
@@ -2,49 +2,52 @@
 
 import forEachHtmlNodes from '@inc2734/for-each-html-nodes';
 
-export default class SnowMonkeyWidgetItemExpander {
+const open  = (element) => element.setAttribute('data-is-expanded', 'true');
+const close = (element) => element.setAttribute('data-is-expanded', 'false');
+const show  = (element) => element.setAttribute('data-is-hidden', 'false');
+const hide  = (element) => element.setAttribute('data-is-hidden', 'true');
 
+const insertExpander = (submenu) => {
+  const parent = submenu.parentNode;
+
+  const toggle = (btn) => {
+    const _open = () => {
+      open(btn);
+
+      const children = parent.children;
+      forEachHtmlNodes(children, (element) => 'true' === element.getAttribute('data-is-hidden') && show(element));
+    };
+
+    const _close = () => {
+      const expander = parent.querySelectorAll('.children-expander');
+      forEachHtmlNodes(expander, (element) => close(element));
+
+      const submenus = parent.querySelectorAll('.children, .sub-menu');
+      forEachHtmlNodes(submenus, (element) => hide(element));
+    };
+
+    'false' === btn.getAttribute('data-is-expanded') ? _open() : _close();
+  };
+
+  const btn = (() => {
+    const btn = document.createElement('button');
+    btn.classList.add('children-expander');
+    close(btn);
+    btn.addEventListener('click', () => toggle(btn), false);
+    return btn;
+  })();
+
+  hide(submenu);
+  parent.insertBefore(btn, parent.firstElementChild);
+};
+
+export default class SnowMonkeyWidgetItemExpander {
   constructor() {
-    window.addEventListener('DOMContentLoaded', () => this._DOMContentLoaded(), false);
+    document.addEventListener('DOMContentLoaded', () => this._DOMContentLoaded(), false);
   }
 
   _DOMContentLoaded() {
     const children = document.querySelectorAll('.c-widget .children, .c-widget .sub-menu');
-
-    forEachHtmlNodes(children, (element) => {
-      element.setAttribute('data-is-hidden', 'true');
-
-      const parent = element.parentNode;
-      const btn = document.createElement('button');
-      btn.classList.add('children-expander');
-      btn.setAttribute('data-is-expanded', 'false');
-
-      parent.insertBefore(btn, parent.firstElementChild);
-
-      btn.addEventListener('click', (event) => this._click(event), false);
-    });
-  }
-
-  _click(event) {
-    const btn = event.currentTarget;
-    const parent = btn.parentNode;
-
-    if ('false' === btn.getAttribute('data-is-expanded')) {
-      btn.setAttribute('data-is-expanded', 'true');
-
-      forEachHtmlNodes(parent.children, (element) => {
-        if ('true' === element.getAttribute('data-is-hidden')) {
-          element.setAttribute('data-is-hidden', 'false');
-        }
-      });
-    } else {
-      forEachHtmlNodes(parent.getElementsByClassName('children-expander'), (element) => {
-        element.setAttribute('data-is-expanded', 'false');
-      });
-
-      forEachHtmlNodes(parent.querySelectorAll('.children, .sub-menu'), (element) => {
-        element.setAttribute('data-is-hidden', 'true');
-      });
-    }
+    forEachHtmlNodes(children, (element) => insertExpander(element));
   }
 }
