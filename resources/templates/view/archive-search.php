47190cebd51d9d2a551986600b591ce2aba5b113
Takashi Kitajima <inc@2inc.org>
2019-03-22 12:13:26 +0900

    Fix #480

---
+++ resources/templates/view/archive-search.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package snow-monkey
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+
+use Framework\Helper;
+
+$post_type = filter_input( INPUT_GET, 'post_type' );
+$post_type = $post_type ? $post_type : 'post';
+?>
+<div class="c-entry">
+	<header class="c-entry__header">
+		<h1 class="c-entry__title"><?php echo esc_html( Helper::get_page_title_from_breadcrumbs() ); ?></h1>
+	</header>
+
+	<div class="c-entry__body">
+		<div class="c-entry__content p-entry-content">
+			<div class="p-archive">
+				<ul class="c-entries c-entries--<?php echo esc_attr( get_theme_mod( $post_type . '-entries-layout' ) ); ?>">
+					<?php while ( have_posts() ) : ?>
+						<?php the_post(); ?>
+						<li class="c-entries__item">
+							<?php Helper::get_template_part( 'template-parts/loop/entry-summary', $post_type ); ?>
+						</li>
+					<?php endwhile; ?>
+				</ul>
+			</div>
+		</div>
+
+		<?php Helper::get_template_part( 'template-parts/archive/pagination' ); ?>
+	</div>
+</div>
