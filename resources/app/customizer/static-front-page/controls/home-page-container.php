70e9116e846671e62f617a89d8ec82c1643a9fcb
Takashi Kitajima <inc@2inc.org>
2019-04-08 12:51:13 +0900

    Fix #492

---
+++ resources/app/customizer/static-front-page/controls/home-page-container.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package snow-monkey
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+
+use Inc2734\WP_Customizer_Framework\Framework;
+
+Framework::control(
+	'checkbox',
+	'home-page-container',
+	[
+		'label'           => __( 'Add side padding to content area of homepage', 'snow-monkey' ),
+		'default'         => true,
+		'active_callback' => function() {
+			$page_on_front        = get_option( 'page_on_front' );
+			$use_static_frontpage = 'page' === get_option( 'show_on_front' ) && ! empty( $page_on_front );
+
+			$wp_page_template         = get_post_meta( $page_on_front, '_wp_page_template', true );
+			$is_default_page_template = 'default' === $wp_page_template;
+			$is_one_column_full       = false !== strpos( $wp_page_template, 'one-column-full.php' );
+
+			$active = ( ! $wp_page_template || $is_default_page_template || $is_one_column_full ) && $use_static_frontpage;
+
+
+			return $active;
+		},
+	]
+);
+
+if ( ! is_customize_preview() ) {
+	return;
+}
+
+$section = Framework::get_section( 'static_front_page' );
+$control = Framework::get_control( 'home-page-container' );
+$control->join( $section );
--- resources/templates/view/front-page.php
+++ resources/templates/view/front-page.php
@@ -17,33 +17,42 @@ $content = ob_get_clean();
 
 <?php if ( $content ) : ?>
 	<?php
-	$wp_page_template = get_post_meta( get_the_ID(), '_wp_page_template', true );
-
 	$classes = [ 'c-section', 'p-section-front-page-content' ];
 	if ( ! get_theme_mod( 'home-page-content-padding' ) ) {
 		$classes[] = 'p-section-front-page-content--no-vpadding';
 	}
 
-	$require_container = ! $wp_page_template
-										|| 'default' === $wp_page_template
-										|| false !== strpos( $wp_page_template, 'one-column-full.php' );
+	$wp_page_template         = get_post_meta( get_the_ID(), '_wp_page_template', true );
+	$use_home_page_container  = get_theme_mod( 'home-page-container' );
+	$is_default_page_template = 'default' === $wp_page_template;
+	$is_one_column_full       = false !== strpos( $wp_page_template, 'one-column-full.php' );
+
+	$require_container = ( ! $wp_page_template || $is_default_page_template || $is_one_column_full ) && $use_home_page_container;
 	?>
 
 	<div class="<?php echo esc_attr( implode( ' ', $classes ) ); ?>">
 		<?php if ( $require_container ) : ?>
-			<div class="c-container">
-		<?php endif; ?>
 
-		<div <?php post_class(); ?>>
-			<div class="c-entry__body">
-				<div class="c-entry__content p-entry-content">
-					<?php the_content(); ?>
+			<div class="c-container">
+				<div <?php post_class(); ?>>
+					<div class="c-entry__body">
+						<div class="c-entry__content p-entry-content">
+							<?php the_content(); ?>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<?php if ( $require_container ) : ?>
+		<?php else : ?>
+
+			<div <?php post_class(); ?>>
+				<div class="c-entry__body">
+					<div class="c-entry__content p-entry-content">
+						<?php the_content(); ?>
+					</div>
+				</div>
 			</div>
+
 		<?php endif; ?>
 	</div>
 <?php endif; ?>
