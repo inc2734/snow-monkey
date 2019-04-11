509c54fb6d5dc9d76b0199a70fdfa2609d512b1a
Takashi Kitajima <inc@2inc.org>
2019-04-11 09:48:22 +0900

    Fix #496

--- resources/src/css/foundation/_element/_element.php
+++ resources/src/css/foundation/_element/_element.php
@@ -11,3 +11,17 @@ Style::register(
 	'a',
 	'color: ' . get_theme_mod( 'accent-color' )
 );
+
+Style::register(
+	[
+		'input[type="email"]',
+		'input[type="number"]',
+		'input[type="password"]',
+		'input[type="search"]',
+		'input[type="tel"]',
+		'input[type="text"]',
+		'input[type="url"]',
+		'textarea',
+	],
+	'font-size: ' . get_theme_mod( 'base-font-size' ) . 'px'
+);
