c09cfbe681c8f7983ce3f1354cea60a87b9b095b
Takashi Kitajima <inc@2inc.org>
2019-04-23 11:30:13 +0900

    Fix #502

--- resources/woocommerce/loop/orderby.php
+++ resources/woocommerce/loop/orderby.php
@@ -11,9 +11,8 @@
  * the readme will list any important changes.
  *
  * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     3.3.0
+ * @version     3.6.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -23,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <form class="woocommerce-ordering" method="get">
 	<span class="c-select" aria-selected="false">
-		<select name="orderby" class="orderby">
+		<select name="orderby" class="orderby" aria-label="<?php esc_attr_e( 'Shop order', 'woocommerce' ); ?>">
 			<?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
 				<option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
 			<?php endforeach; ?>
