f1af7cb501c462852663256e76d6d316cf22f8b5
Takashi Kitajima <inc@2inc.org>
2019-06-13 22:56:17 +0900

    ブログカードのデザインを調整

--- resources/app/setup/oembed.php
+++ resources/app/setup/oembed.php
@@ -9,3 +9,25 @@
 use Inc2734\WP_OEmbed_Blog_Card\Bootstrap;
 
 new Bootstrap();
+
+add_filter(
+	'wp_oembed_blog_card_loading_template',
+	function( $html ) {
+		return str_replace(
+			'class="js-wp-oembed-blog-card"',
+			'class="js-wp-oembed-blog-card wp-oembed-blog-card"',
+			$html
+		);
+	}
+);
+
+add_filter(
+	'wp_oembed_blog_card_url_template',
+	function( $html ) {
+		return str_replace(
+			'class="wp-oembed-blog-card-url-template"',
+			'class="wp-oembed-blog-card-url-template wp-oembed-blog-card"',
+			$html
+		);
+	}
+);
