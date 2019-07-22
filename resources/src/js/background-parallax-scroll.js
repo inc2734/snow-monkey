dabe36b37bdd5cbcaa6aaf113d30741941dd3b06
Takashi Kitajima <inc@2inc.org>
2019-07-22 11:28:40 +0900

    Some fixes

--- resources/src/js/background-parallax-scroll.js
+++ resources/src/js/background-parallax-scroll.js
@@ -3,5 +3,4 @@
 import $ from 'jquery';
 import '../../assets/packages/jquery.background-parallax-scroll';
 
-console.log($('.js-bg-parallax').length);
 $('.js-bg-parallax').backgroundParallaxScroll();
