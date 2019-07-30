4a9c971aad380292388e5c074711f9f594021719
Takashi Kitajima <inc@2inc.org>
2019-07-30 10:16:07 +0900

    Fix #594

--- resources/src/js/page-top.js
+++ resources/src/js/page-top.js
@@ -4,6 +4,9 @@ import PageTopBtn from './module/_pagetop-btn.js';
 
 document.addEventListener(
   'DOMContentLoaded',
-  () => new PageTopBtn(document.querySelector('.c-page-top')),
+  () => {
+    const pageTop = document.querySelector('.c-page-top');
+    pageTop && new PageTopBtn(pageTop);
+  },
   false
 );
