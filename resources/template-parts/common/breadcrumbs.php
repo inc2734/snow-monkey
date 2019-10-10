907998985b11060a4741e084f762c89a2311ad41
Takashi Kitajima <inc@2inc.org>
2019-10-10 23:42:55 +0900

    Fix #637

--- resources/template-parts/common/breadcrumbs.php
+++ resources/template-parts/common/breadcrumbs.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 7.0.0
+ * @version 8.0.0
  *
  * renamed: template-parts/breadcrumbs.php
  */
@@ -21,13 +21,15 @@ if ( ! $template_args['items'] ) {
 }
 ?>
 
-<ol class="c-breadcrumbs" itemscope itemtype="http://schema.org/BreadcrumbList">
-	<?php foreach ( $template_args['items'] as $key => $item ) : ?>
-		<li class="c-breadcrumbs__item" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-			<a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="<?php echo esc_url( $item['link'] ); ?>" itemid="<?php echo esc_url( $item['link'] ); ?>">
-				<span itemprop="name"><?php echo esc_html( strip_tags( $item['title'] ) ); ?></span>
-			</a>
-			<meta itemprop="position" content="<?php echo esc_attr( $key + 1 ); ?>" />
-		</li>
-	<?php endforeach; ?>
-</ol>
+<div class="p-breadcrumbs-wrapper">
+	<ol class="c-breadcrumbs" itemscope itemtype="http://schema.org/BreadcrumbList">
+		<?php foreach ( $template_args['items'] as $key => $item ) : ?>
+			<li class="c-breadcrumbs__item" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+				<a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="<?php echo esc_url( $item['link'] ); ?>" itemid="<?php echo esc_url( $item['link'] ); ?>">
+					<span itemprop="name"><?php echo esc_html( strip_tags( $item['title'] ) ); ?></span>
+				</a>
+				<meta itemprop="position" content="<?php echo esc_attr( $key + 1 ); ?>" />
+			</li>
+		<?php endforeach; ?>
+	</ol>
+</div>
