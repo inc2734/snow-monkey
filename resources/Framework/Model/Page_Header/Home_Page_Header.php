187b963c74684f05068da4c1af9aadb0f758c7de
Takashi Kitajima <inc@2inc.org>
2019-10-15 13:28:14 +0900

    Fix #629

--- resources/Framework/Model/Page_Header/Home_Page_Header.php
+++ resources/Framework/Model/Page_Header/Home_Page_Header.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 5.0.2
+ * @version 8.0.0
  */
 
 namespace Framework\Model\Page_Header;
@@ -18,7 +18,7 @@ class Home_Page_Header extends Base {
 	 * @return string
 	 */
 	public static function get_image_url() {
-		if ( 'page' === get_option( 'show_on_front' ) ) {
+		if ( 'page' === get_option( 'show_on_front' ) && get_option( 'page_for_posts' ) ) {
 			$thumbnail_id = get_post_thumbnail_id( get_option( 'page_for_posts' ) );
 			if ( $thumbnail_id ) {
 				return wp_get_attachment_image_url( $thumbnail_id, static::_get_thumbnail_size() );
