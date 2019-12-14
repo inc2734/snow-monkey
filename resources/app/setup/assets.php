3690a205a37bc1a77d5b0135f3b4dda1c1c8c0ad
Takashi Kitajima <inc@2inc.org>
2019-12-14 09:55:58 +0900

    Fix #680

--- resources/app/setup/assets.php
+++ resources/app/setup/assets.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 8.3.2
+ * @version 8.4.0
  */
 
 use Framework\Helper;
--- resources/templates/layout/header/2row.php
+++ resources/templates/layout/header/2row.php
@@ -5,7 +5,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 8.2.0
+ * @version 8.4.0
  */
 
 use Framework\Helper;
@@ -16,7 +16,15 @@ $header_position_fixed    = Helper::get_header_position_fixed();
 $scrolling_header_colored = Helper::get_scrolling_header_colored();
 ?>
 <header class="l-header" role="banner" data-l-header-type="<?php echo esc_attr( $header_position ); ?>" data-snow-monkey-default-header-position="<?php echo esc_attr( $default_header_position ); ?>" data-snow-monkey-header-position-fixed="<?php echo esc_attr( $header_position_fixed ); ?>" data-snow-monkey-scrolling-header-colored="<?php echo esc_attr( $scrolling_header_colored ); ?>">
-	<?php Helper::get_template_part( 'template-parts/header/2row' ); ?>
+	<?php
+	if ( get_theme_mod( 'infobar-content' ) && 'overlay' === get_theme_mod( 'header-position' ) ) {
+		Helper::get_template_part( 'template-parts/common/infobar' );
+	}
+	?>
+
+	<div class="l-header__content">
+		<?php Helper::get_template_part( 'template-parts/header/2row' ); ?>
+	</div>
 
 	<?php if ( has_nav_menu( 'global-nav' ) ) : ?>
 		<div class="l-header__drop-nav" aria-hidden="true">
