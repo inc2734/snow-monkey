0f7b1a54f2b45da7f87d6d479375556906c7d7af
Takashi Kitajima <inc@2inc.org>
2020-04-30 15:22:40 +0900

    Fix #765

--- resources/app/customizer/page-speed-optimization/controls/browser-cache.php
+++ resources/app/customizer/page-speed-optimization/controls/browser-cache.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 9.0.0
+ * @version 10.2.2
  */
 
 use Inc2734\WP_Customizer_Framework\Framework;
@@ -15,7 +15,7 @@ Framework::control(
 	'set-browser-cache',
 	[
 		'label'       => __( 'Use browser cache', 'snow-monkey' ),
-		'description' => __( 'The server must support htaccess.', 'snow-monkey' ),
+		'description' => __( 'The server must support htaccess, IfModule and mod_expires. If you get the Internal Server Error (500), delete the characters added to .htaccess.', 'snow-monkey' ),
 		'type'        => 'option',
 		'priority'    => 140,
 		'default'     => false,
