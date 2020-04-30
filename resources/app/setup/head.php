5fa9f3f6ef78c8a176ecd527ba9231313beb625f
Takashi Kitajima <inc@2inc.org>
2020-04-30 15:26:15 +0900

    fix #762

--- resources/app/setup/head.php
+++ resources/app/setup/head.php
@@ -3,11 +3,13 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 9.0.0
+ * @version 10.2.2
  */
 
 /**
- * Sets up content of the head element
+ * Sets up charset and viewport
+ *
+ * @see https://github.com/joshbuchea/head#recommended-minimum
  *
  * @return void
  */
@@ -17,11 +19,25 @@ add_action(
 		?>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, viewport-fit=cover">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
-			<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-		<?php endif; ?>
 		<?php
 	},
-	1
+	0.9
+);
+
+/**
+* Sets up content of the head element
+*
+* @return void
+*/
+add_action(
+ 'wp_head',
+ function() {
+	 ?>
+	 <link rel="profile" href="http://gmpg.org/xfn/11">
+	 <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+		 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	 <?php endif; ?>
+	 <?php
+ },
+ 2
 );
