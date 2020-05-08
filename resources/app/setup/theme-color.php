cb6b4384a58c3b65b6884bf7b0e168d83b6540a3
Takashi Kitajima <inc@2inc.org>
2020-05-08 16:32:56 +0900

    fix #775

--- resources/app/setup/theme-color.php
+++ resources/app/setup/theme-color.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 10.2.4
+ * @version 10.4.0
  */
 
 /**
@@ -14,12 +14,12 @@
 add_action(
 	'wp_head',
 	function() {
-		$accent_color = get_theme_mod( 'accent-color' );
-		if ( ! $accent_color ) {
+		$theme_color = apply_filters( 'snow_monkey_theme_color', get_theme_mod( 'accent-color' ) );
+		if ( ! $theme_color ) {
 			return;
 		}
 		?>
-		<meta name="theme-color" content="<?php echo esc_attr( $accent_color ); ?>">
+		<meta name="theme-color" content="<?php echo esc_attr( $theme_color ); ?>">
 		<?php
 	}
 );
