ea9cf07303cd55d971d063b20a1444feae7ea9ff
Takashi Kitajima <inc@2inc.org>
2020-08-03 15:09:00 +0900

    fix #860

--- resources/template-parts/nav/social.php
+++ resources/template-parts/nav/social.php
@@ -14,18 +14,20 @@ if ( ! has_nav_menu( 'social-nav' ) ) {
 ?>
 
 <nav class="p-social-nav" role="navigation">
-	<?php
-	wp_nav_menu(
-		[
-			'theme_location' => 'social-nav',
-			'container'      => false,
-			'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-			'menu_class'     => 'c-navbar',
-			'depth'          => 1,
-			'link_before'    => '<span>',
-			'link_after'     => '</span>',
-			'walker'         => new \Inc2734\WP_Basis\App\Walker\Navbar(),
-		]
-	);
-	?>
+	<div class="p-social-nav__inner">
+		<?php
+		wp_nav_menu(
+			[
+				'theme_location' => 'social-nav',
+				'container'      => false,
+				'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				'menu_class'     => 'c-navbar',
+				'depth'          => 1,
+				'link_before'    => '<span>',
+				'link_after'     => '</span>',
+				'walker'         => new \Inc2734\WP_Basis\App\Walker\Navbar(),
+			]
+		);
+		?>
+	</div>
 </nav>
