6f455db9a21fe309eae3c2360fc3dda7d5f779f0
Takashi Kitajima <inc@2inc.org>
2020-08-17 12:42:03 +0900

    fix #869

---
+++ resources/src/css/foundation/_wordpress/_wp-block-media-text/_wp-block-media-text.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package snow-monkey
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+
+use Inc2734\WP_Customizer_Framework\Style;
+
+Style::extend( 'entry-content', [ '.wp-block-media-text__content' ] );
