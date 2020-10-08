4e8a9260eb13d94455b1fbba2cef59c7a81fb674
inc2734 <inc@2inc.org>
2020-10-08 09:14:13 +0900

    fix #916

--- app/constructor/customizer.php
+++ app/constructor/customizer.php
@@ -3,9 +3,14 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 9.0.0
+ * @version 11.5.7
  */
 
 use Inc2734\WP_Customizer_Framework\Bootstrap;
+use Framework\Helper;
 
-new Bootstrap();
+new Bootstrap(
+	[
+		'handle' => Helper::get_main_style_handle(),
+	]
+);
