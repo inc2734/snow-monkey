21d6420ceb70f06a24c5a7a2bfdd02d3dac5a206
inc2734 <inc@2inc.org>
2020-11-04 11:14:54 +0900

    fix #951

--- src/js/smooth-scroll.js
+++ src/js/smooth-scroll.js
@@ -1,6 +1,9 @@
 import { SmoothScroll } from '@inc2734/smooth-scroll';
 import { getScrollOffset } from './module/_helper';
 
+class SnowMonkeySmoothScroll extends SmoothScroll {
+}
+
 window.addEventListener(
   'load',
   () => {
@@ -10,7 +13,7 @@ window.addEventListener(
       '.u-smooth-scroll[href*="#"]',
       '.u-smooth-scroll a[href*="#"]',
     ].forEach((selector) => {
-      new SmoothScroll(
+      new SnowMonkeySmoothScroll(
         {
           selector,
           offset: () => getScrollOffset(),
