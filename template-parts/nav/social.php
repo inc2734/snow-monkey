7890fb0b28fde1c62bd712e494cee2ef17d5af4b
inc2734 <inc@2inc.org>
2020-11-28 14:50:52 +0900

    fix #959

--- template-parts/nav/social.php
+++ template-parts/nav/social.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 11.0.0
+ * @version 12.0.0
  *
  * renamed: template-parts/social-nav.php
  */
@@ -23,7 +23,7 @@ if ( ! has_nav_menu( 'social-nav' ) ) {
 				'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
 				'menu_class'     => 'c-navbar',
 				'depth'          => 1,
-				'link_before'    => '<span>',
+				'link_before'    => '<span class="screen-reader-text">',
 				'link_after'     => '</span>',
 				'walker'         => new \Inc2734\WP_Basis\App\Walker\Navbar(),
 			]
