6d8cae9393f9101a0e71396695fc71f7ccb4b288
inc2734 <inc@2inc.org>
2020-12-03 03:06:07 +0900

    fix #963

--- template-parts/common/profile-box.php
+++ template-parts/common/profile-box.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 11.3.3
+ * @version 12.0.0
  *
  * renamed: template-parts/profile-box.php
  */
@@ -13,8 +13,15 @@ $args = wp_parse_args(
 	$args,
 	// phpcs:enable
 	[
-		'_title' => __( 'Bio', 'snow-monkey' ),
+		'_title'             => __( 'Bio', 'snow-monkey' ),
+		'_in_same_post_type' => false,
 	]
 );
 
-echo do_shortcode( '[wp_profile_box title="' . $args['_title'] . '"]' );
+echo do_shortcode(
+	sprintf(
+		'[wp_profile_box title="%1$s" in_same_post_type="%2$s"]',
+		$args['_title'],
+		$args['_in_same_post_type']
+	)
+);
