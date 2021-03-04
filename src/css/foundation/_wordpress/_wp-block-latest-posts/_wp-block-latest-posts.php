09a29f203a310ec8a89dafa0f64657f24f12ffa3
inc2734 <inc@2inc.org>
2021-03-04 14:16:25 +0900

    fix #1024

---
+++ src/css/foundation/_wordpress/_wp-block-latest-posts/_wp-block-latest-posts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package snow-monkey
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+
+use Framework\Helper;
+use Inc2734\WP_Customizer_Framework\Style;
+
+if ( ! Helper::is_ie() ) {
+	return;
+}
+
+Style::extend( 'entry-content', [ '.wp-block-latest-posts__post-full-content' ] );
