532c5295f788e3476296084f9148101a26d6ed03
inc2734 <inc@2inc.org>
2021-03-15 21:14:22 +0900

    fix #1035

--- app/customizer/design/sections/base-design/controls/container-max-width.php
+++ app/customizer/design/sections/base-design/controls/container-max-width.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 9.0.0
+ * @version 14.0.0
  *
  * renamed: app/customizer/layout/sections/base-layout/controls/container-max-width.php
  */
@@ -15,13 +15,18 @@ Framework::control(
 	'container-max-width',
 	[
 		'label'       => __( 'Contents max width', 'snow-monkey' ),
-		'description' => __( 'You can set max width of contents area (1024 - 1280)', 'snow-monkey' ),
+		'description' => sprintf(
+			// translators: %1$s: min width, %2$s: max width
+			__( 'You can set max width of contents area (%1$s - %2$s)', 'snow-monkey' ),
+			1024,
+			1600
+		),
 		'priority'    => 140,
 		'default'     => '1280',
 		'input_attrs' => [
 			'step' => 1,
 			'min'  => 1024,
-			'max'  => 1280,
+			'max'  => 1600,
 		],
 	]
 );
