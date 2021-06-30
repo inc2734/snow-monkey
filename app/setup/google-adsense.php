1907d50da3375ee72b812bfba65b6e753d2bfac0
inc2734 <inc@2inc.org>
2021-06-30 14:21:12 +0900

    fix #1092

--- app/setup/google-adsense.php
+++ app/setup/google-adsense.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 11.4.0
+ * @version 15.0.0
  */
 
 use Framework\Helper;
@@ -133,7 +133,7 @@ add_filter(
 add_filter(
 	'inc2734_wp_adsense_the_adsense_code',
 	function( $code ) {
-		if ( ! is_customize_preview() ) {
+		if ( ! is_customize_preview() && ! is_preview() ) {
 			return $code;
 		}
 
