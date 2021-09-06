6c03ee0120156ba0244bb5a6fd720e61880b1871
inc2734 <inc@2inc.org>
2021-09-06 10:38:44 +0900

    fix #1147

--- src/css/dependency/snow-monkey-blocks/tabs/block.php
+++ src/css/dependency/snow-monkey-blocks/tabs/block.php
@@ -12,4 +12,17 @@ if ( ! Helper::is_ie() ) {
 	return;
 }
 
+$accent_color = get_theme_mod( 'accent-color' );
+if ( $accent_color ) {
+	$styles[] = [
+		'selectors'  => [ '.smb-tabs.is-style-line > .smb-tabs__tabs .smb-tabs__tab::after' ],
+		'properties' => [ 'color: ' . $accent_color ],
+	];
+
+	Style::attach(
+		Helper::get_main_style_handle() . '-snow-monkey-blocks',
+		$styles
+	);
+}
+
 Style::extend( 'entry-content', [ '.smb-tab-panel__summary' ] );
