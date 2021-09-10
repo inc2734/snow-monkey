f77c3f2f53d976e086de517becef7594de422406
inc2734 <inc@2inc.org>
2021-09-10 12:20:46 +0900

    Fix #1156

--- app/customizer/design/sections/bbpress-archive-page/controls/page-header-align.php
+++ app/customizer/design/sections/bbpress-archive-page/controls/page-header-align.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 15.7.0
+ * @version 15.7.1
  */
 
 use Inc2734\WP_Customizer_Framework\Framework;
@@ -11,14 +11,14 @@ use Framework\Helper;
 
 Framework::control(
 	'select',
-	'woocommerce-archive-page-header-align',
+	'bbpress-archive-page-header-align',
 	[
 		'label'           => __( 'Page header alignment', 'snow-monkey' ),
 		'priority'        => 121,
 		'default'         => 'center',
 		'choices'         => Helper::page_header_align_choices(),
 		'active_callback' => function() {
-			return 'title-on-page-header' === get_theme_mod( 'woocommerce-archive-eyecatch' );
+			return 'title-on-page-header' === get_theme_mod( 'bbpress-archive-eyecatch' );
 		},
 	]
 );
@@ -28,6 +28,6 @@ if ( ! is_customize_preview() ) {
 }
 
 $panel   = Framework::get_panel( 'design' );
-$section = Framework::get_section( 'design-woocommerce-archive-page' );
-$control = Framework::get_control( 'woocommerce-archive-page-header-align' );
+$section = Framework::get_section( 'design-bbpress-archive-page' );
+$control = Framework::get_control( 'bbpress-archive-page-header-align' );
 $control->join( $section )->join( $panel );
