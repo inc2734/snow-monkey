d50131ff4be0fc8622d276bd6c2c3d0e65e27a3e
inc2734 <inc@2inc.org>
2021-10-02 02:34:05 +0900

    #1166 見出し/ウィジェットタイトルのデザイン変更は theme が enqueue されているときのみ表示

--- app/customizer/design/sections/base-design/controls/h2-style.php
+++ app/customizer/design/sections/base-design/controls/h2-style.php
@@ -3,23 +3,28 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 11.0.8
+ * @version 15.9.0
  */
 
 use Inc2734\WP_Customizer_Framework\Framework;
+use Framework\Helper;
 
 Framework::control(
 	'select',
 	'h2-style',
 	[
 		// translators: %1$s: heading element
-		'label'    => sprintf( __( 'Design of the %1$s in articles', 'snow-monkey' ), 'h2' ),
-		'priority' => 270,
-		'default'  => 'standard',
-		'choices'  => [
+		'label'           => sprintf( __( 'Design of the %1$s in articles', 'snow-monkey' ), 'h2' ),
+		'priority'        => 270,
+		'default'         => 'standard',
+		'choices'         => [
 			''         => __( 'None', 'snow-monkey' ),
 			'standard' => __( 'Standard', 'snow-monkey' ),
 		],
+		'active_callback' => function() {
+			$handle = Helper::get_main_style_handle() . '-theme';
+			return wp_style_is( $handle ) && wp_styles()->registered[ $handle ]->src;
+		},
 	]
 );
 
--- app/customizer/design/sections/base-design/controls/widget-title-style.php
+++ app/customizer/design/sections/base-design/controls/widget-title-style.php
@@ -3,22 +3,28 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 11.0.8
+ * @version 15.9.0
  */
 
 use Inc2734\WP_Customizer_Framework\Framework;
+use Framework\Helper;
+
 
 Framework::control(
 	'select',
 	'widget-title-style',
 	[
-		'label'    => __( 'Design of widget-titles', 'snow-monkey' ),
-		'priority' => 280,
-		'default'  => 'standard',
-		'choices'  => [
+		'label'           => __( 'Design of widget-titles', 'snow-monkey' ),
+		'priority'        => 280,
+		'default'         => 'standard',
+		'choices'         => [
 			''         => __( 'None', 'snow-monkey' ),
 			'standard' => __( 'Standard', 'snow-monkey' ),
 		],
+		'active_callback' => function() {
+			$handle = Helper::get_main_style_handle() . '-theme';
+			return wp_style_is( $handle ) && wp_styles()->registered[ $handle ]->src;
+		},
 	]
 );
 
