f6dce9c6c318195faec0cabc304d3e1d41ba4a42
inc2734 <inc@2inc.org>
2021-11-16 10:21:54 +0900

    fix #1187

---
+++ app/setup/document-title.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package snow-monkey
+ * @author inc2734
+ * @license GPL-2.0+
+ * @version 15.15.0
+ */
+
+/**
+ * Customize for search result.
+ * @param array $title {
+ *     The document title parts.
+ *     @type string $title   Title of the viewed page.
+ *     @type string $page    Optional. Page number if paginated.
+ *     @type string $tagline Optional. Site description when on home page.
+ *     @type string $site    Optional. Site title when not on home page.
+ */
+add_filter(
+	'document_title_parts',
+	function( $title ) {
+		if ( is_search() && ! get_search_query() ) {
+			$title['title'] = __( 'Search results', 'snow-monkey' );
+		}
+		return $title;
+	}
+);
--- search.php
+++ search.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 15.0.0
+ * @version 15.15.0
  */
 
 use Framework\Controller\Controller;
@@ -29,7 +29,7 @@ $layout = get_theme_mod( 'archive-' . $_post_type . '-layout' );
 $layout = $layout ? $layout : get_theme_mod( 'archive-post-layout' );
 
 Controller::layout( $layout );
-if ( '' === get_search_query() ) {
+if ( ! get_search_query() && ! get_queried_object() ) {
 	Controller::render( 'no-keywords' );
 } elseif ( have_posts() ) {
 	$archive_view = get_theme_mod( $_post_type . '-archive-view' );
