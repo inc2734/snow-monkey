4b5eddcd0c9b9c67268e048effe5b0597b053938
inc2734 <inc@2inc.org>
2023-02-06 09:11:14 +0900

    fix #1296

--- src/js/widgets.js
+++ src/js/widgets.js
@@ -1,5 +1,6 @@
 import forEachHtmlNodes from '@inc2734/for-each-html-nodes';
 import { widgetItemExpander } from './module/_widget-item-expander';
+import { activeMenu } from './module/_active-menu';
 
 document.addEventListener(
   'DOMContentLoaded',
@@ -13,4 +14,27 @@ document.addEventListener(
     forEachHtmlNodes(submenus, widgetItemExpander);
   },
 	false
+);
+
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    const navs = document.querySelectorAll('.wpaw-local-nav__sublist');
+    if (1 > navs.length) {
+      return;
+    }
+
+    forEachHtmlNodes(navs, (nav) => {
+      const links = nav.getElementsByTagName('a');
+
+      const applyActiveMenu = (atag) => {
+        const params = {
+          home_url: snow_monkey.home_url,
+        };
+        activeMenu(atag, params);
+      };
+
+      forEachHtmlNodes(links, applyActiveMenu);
+    });
+  }
 );
