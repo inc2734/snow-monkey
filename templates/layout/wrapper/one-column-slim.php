2a02f8c471319972f62700359e91454754466fca
inc2734 <inc@2inc.org>
2023-02-16 19:23:44 +0900

    #1313 「コンテンツにあわせる」パンくずは .u-slim-width 内に、.c-entry > * に max-width を適用

--- templates/layout/wrapper/one-column-slim.php
+++ templates/layout/wrapper/one-column-slim.php
@@ -5,7 +5,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 19.1.3
+ * @version 19.1.5
  */
 
 use Framework\Helper;
@@ -80,11 +80,11 @@ use Framework\Helper;
 					}
 					?>
 
-					<?php
-					if ( ! is_front_page() && 'content-width' === get_theme_mod( 'breadcrumbs-position' ) ) {
-						Helper::get_template_part( 'template-parts/common/breadcrumbs' );
-					}
-					?>
+					<?php if ( ! is_front_page() && 'content-width' === get_theme_mod( 'breadcrumbs-position' ) ) : ?>
+						<div class="u-slim-width">
+							<?php Helper::get_template_part( 'template-parts/common/breadcrumbs' ); ?>
+						</div>
+					<?php endif; ?>
 
 					<?php do_action( 'snow_monkey_before_contents_inner' ); ?>
 
@@ -104,18 +104,18 @@ use Framework\Helper;
 
 					<?php do_action( 'snow_monkey_after_contents_inner' ); ?>
 
+					<?php if ( ! is_front_page() && 'bottom-content-width' === get_theme_mod( 'breadcrumbs-position' ) ) :?>
+						<div class="u-slim-width">
+							<?php Helper::get_template_part( 'template-parts/common/breadcrumbs' ); ?>
+						</div>
+					<?php endif; ?>
+
 					<?php
-					if ( ! is_front_page() && 'bottom-content-width' === get_theme_mod( 'breadcrumbs-position' ) ) {
+					if ( ! is_front_page() && 'bottom' === get_theme_mod( 'breadcrumbs-position' ) ) {
 						Helper::get_template_part( 'template-parts/common/breadcrumbs' );
 					}
 					?>
 				</div>
-
-				<?php
-				if ( ! is_front_page() && 'bottom' === get_theme_mod( 'breadcrumbs-position' ) ) {
-					Helper::get_template_part( 'template-parts/common/breadcrumbs' );
-				}
-				?>
 			</div>
 
 			<?php do_action( 'snow_monkey_append_contents' ); ?>
