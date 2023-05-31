5b0436eca352b24c3d9072ee1972b733485e9d45
inc2734 <inc@2inc.org>
2023-05-31 14:27:38 +0900

    fix #1347

--- app/setup/oembed.php
+++ app/setup/oembed.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 14.3.0
+ * @version 20.2.0
  */
 
 use Inc2734\WP_OEmbed_Blog_Card\Bootstrap;
@@ -36,5 +36,14 @@ add_filter(
 	'inc2734_wp_oembed_blog_card_cache_directory',
 	function( $directory ) {
 		return apply_filters( 'snow_monkey_oembed_blog_card_cache_directory', $directory );
+	}
+);
+
+add_filter(
+	'inc2734_wp_oembed_blog_card_block_editor_styles',
+	function( $styles ) {
+		$styles[] = get_template_directory_uri() . '/assets/css/app/app.css?ver=' . filemtime( get_template_directory() . '/assets/css/app/app.css' );
+		$styles[] = get_template_directory_uri() . '/assets/css/app/app-theme.css?ver=' . filemtime( get_template_directory() . '/assets/css/app/app-theme.css' );
+		return $styles;
 	}
 );
