272e6d5603b00d707586598cc0f0b4b0444cffaf
inc2734 <inc@2inc.org>
2023-07-12 15:08:33 +0900

    fix #1372

--- template-parts/footer/copyright.php
+++ template-parts/footer/copyright.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 24.5.0
+ * @version 24.5.1
  *
  * renamed: template-parts/copyright.php
  */
@@ -47,12 +47,12 @@ if ( $args['_inverse'] ) {
 		$container_class = $args['_container-fluid'] ? 'c-fluid-container' : 'c-container';
 		?>
 		<div class="<?php echo esc_attr( $container_class ); ?>">
-			<?php echo wp_kses_post( apply_filters( 'the_content', $args['_copyright'] ) ); ?>
+			<?php echo wp_kses_post( do_shortcode( $args['_copyright'] ) ); ?>
 		</div>
 
 	<?php else : ?>
 
-		<?php echo wp_kses_post( apply_filters( 'the_content', $args['_copyright'] ) ); ?>
+		<?php echo wp_kses_post( do_shortcode( $args['_copyright'] ) ); ?>
 
 	<?php endif; ?>
 </div>
