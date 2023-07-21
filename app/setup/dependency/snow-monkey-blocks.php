70cb24820c9905320dfb65d08e7f5a0978810fee
inc2734 <inc@2inc.org>
2023-07-21 17:17:08 +0900

    inc2734/snow-monkey-blocks#738 constrained

--- app/setup/dependency/snow-monkey-blocks.php
+++ app/setup/dependency/snow-monkey-blocks.php
@@ -3,7 +3,7 @@
  * @package snow-monkey
  * @author inc2734
  * @license GPL-2.0+
- * @version 19.0.0-beta1
+ * @version 25.0.0
  */
 
 use Framework\Helper;
@@ -58,44 +58,6 @@ add_action(
 			filemtime( get_theme_file_path( '/assets/js/dependency/snow-monkey-blocks/app.js' ) ),
 			true
 		);
-	}
-);
-
-/**
- * Enqueue Snow Monkey Blocks style in the block editor.
- */
-add_action(
-	'enqueue_block_editor_assets',
-	function() {
-		wp_register_style(
-			Helper::get_main_style_handle() . '-snow-monkey-blocks/editor',
-			false,
-			array(
-				Helper::get_main_style_handle() . '-snow-monkey-blocks-app/editor',
-				Helper::get_main_style_handle() . '-snow-monkey-blocks-theme/editor',
-			)
-		);
-
-		wp_register_style(
-			Helper::get_main_style_handle() . '-snow-monkey-blocks-app/editor',
-			get_theme_file_uri( '/assets/css/dependency/snow-monkey-blocks/editor.css' ),
-			array(
-				Helper::get_main_style_handle() . '-snow-monkey-blocks-app',
-			),
-			filemtime( get_theme_file_path( '/assets/css/dependency/snow-monkey-blocks/editor.css' ) )
-		);
-
-		wp_register_style(
-			Helper::get_main_style_handle() . '-snow-monkey-blocks-theme/editor',
-			get_theme_file_uri( '/assets/css/dependency/snow-monkey-blocks/editor-theme.css' ),
-			array(
-				Helper::get_main_style_handle() . '-snow-monkey-blocks-app/editor',
-				Helper::get_main_style_handle() . '-snow-monkey-blocks-theme',
-			),
-			filemtime( get_theme_file_path( '/assets/css/dependency/snow-monkey-blocks/editor-theme.css' ) )
-		);
-
-		wp_enqueue_style( Helper::get_main_style_handle() . '-snow-monkey-blocks/editor' );
 	}
 );
 
