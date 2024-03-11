729995fc17fb22ade84992757264b68e578ade94
inc2734 <inc@2inc.org>
2024-03-11 16:50:43 +0900

    #1405 wp-env の wp-cli には i18n make-php が未実装だったので、とりあえず自作 PHP で生成

---
+++ bin/l10n.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package snow-monkey
+ * @author inc2734
+ * @license GPL-2.0+
+ */
+
+require_once( './wp-load.php' );
+
+$mo_files = glob( __DIR__ . '/../languages/*.mo' );
+foreach ( $mo_files as $mo_file ) {
+	$mo = WP_Translation_File::transform( $mo_file, 'php' );
+	if ( $mo ) {
+		file_put_contents( path_join( dirname( $mo_file ), basename( $mo_file, '.mo' ) ) . '.l10n.php', $mo );
+	}
+}
