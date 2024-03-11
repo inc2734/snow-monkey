65dc172cb0e765240f915a30958968731df25513
inc2734 <inc@2inc.org>
2024-03-11 17:00:04 +0900

    Some fixes

--- bin/l10n.php
+++ bin/l10n.php
@@ -9,7 +9,7 @@ require_once( './wp-load.php' );
 
 $mo_files = glob( __DIR__ . '/../languages/*.mo' );
 foreach ( $mo_files as $mo_file ) {
-	$mo = WP_Translation_File::transform( $mo_file, 'php' );
+	$mo = \WP_Translation_File::transform( $mo_file, 'php' );
 	if ( $mo ) {
 		file_put_contents( path_join( dirname( $mo_file ), basename( $mo_file, '.mo' ) ) . '.l10n.php', $mo );
 	}
